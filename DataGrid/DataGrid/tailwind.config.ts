a64038f8f839bcbd5f57d55cf931489f3a417e79
Aasthakothari17 <kothariaastha0319@gmail.com>
2025-08-17 17:19:14 +0530

    Update tailwind.config.ts

--- DataGrid/DataGrid/tailwind.config.ts
+++ DataGrid/DataGrid/tailwind.config.ts
@@ -13,6 +13,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        background: "#ffffff"
         card: {
           DEFAULT: "var(--card)",
           foreground: "var(--card-foreground)",
